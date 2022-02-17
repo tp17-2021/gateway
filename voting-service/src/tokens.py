660e8dbbe82e9a0d1c230dde473e01fcba8940da
Marek Ceľuch <celuchmarek@gmail.com>
2022-02-17 14:47:02 +0100

    EV-10 Save vote to db, validate token

--- voting-service/src/tokens.py
+++ voting-service/src/tokens.py
@@ -1,19 +1,21 @@
 from fastapi import HTTPException, status
+import requests
+import os
 
 
-def validate_token (token: str) -> None:
+def validate_token (token: str):
     """ Raises HTTPException: 403 if token is invalid. """
 
-    if token == 'valid':
-        return
-
-    raise HTTPException(
-        status_code=status.HTTP_403_FORBIDDEN,
-        detail='Invalid token'
+    return requests.post(
+        os.environ['TOKEN_MANAGER_URL'] + '/tokens/validate',
+        json={'token': token}
     )
 
 
 def use_token (token: str) -> None:
     """ Invalidates token. """
 
-    pass
+    requests.post(
+        os.environ['TOKEN_MANAGER_URL'] + '/tokens/deactivate',
+        json={'token': token}
+    ).raise_for_status()

6bf4c039d31ddec62b3761b72c90d11660943960
Marek Ceľuch <celuchmarek@gmail.com>
2022-02-21 15:43:16 +0100

    EV-99 No need to clean db test with --renew-anon-volumes flag

--- voting-process-manager/tests/conftest.py
+++ voting-process-manager/tests/conftest.py
@@ -4,16 +4,6 @@ import os
 import pymongo
 
 
-def pytest_sessionstart(session):
-    client2 = pymongo.MongoClient(
-        f"{os.environ['KEYS_DB_HOST']}:{os.environ['KEYS_DB_PORT']}"
-    )[os.environ["KEYS_DB_NAME"]][os.environ["KEYS_DB_COLLECTION"]]
-    
-    print('DELETING KEYS DB')
-    client2.delete_many({})
-    print('KEYS DB DELETED')
-
-
 @pytest.fixture
 def event_loop():
     yield asyncio.get_event_loop()
--- voting-service/tests/conftest.py
+++ voting-service/tests/conftest.py
@@ -6,25 +6,10 @@ from electiersa import electiersa
 
 
 def pytest_sessionstart(session):
-    print('DELETING VOTE DB')
-    
-    client = pymongo.MongoClient(
-        f"{os.environ['VOTE_DB_HOST']}:{os.environ['VOTE_DB_PORT']}"
-    )[os.environ["VOTE_DB_DB_NAME"]][os.environ["VOTE_DB_COLLECTION"]]
-    
-    client.delete_many({})
-
-    print('VOTE DB DELETED')
-
-    
     client2 = pymongo.MongoClient(
         f"{os.environ['KEYS_DB_HOST']}:{os.environ['KEYS_DB_PORT']}"
     )[os.environ["KEYS_DB_NAME"]][os.environ["KEYS_DB_COLLECTION"]]
     
-    print('DELETING KEYS DB')
-    client2.delete_many({})
-    print('KEYS DB DELETED')
-
     private_key, public_key = electiersa.get_rsa_key_pair()
     
     print('INSERTING VT KEYS')

36989d2e949c070ae6deba36999493c8dac4db06
liborduda <libor.duda@gmail.com>
2022-02-25 14:01:25 +0100

    EV-131 start and stop election add endpoints

--- admin-frontend/src/api/api.ts
+++ admin-frontend/src/api/api.ts
@@ -16,3 +16,38 @@ export async function getVTStatuses(): Promise<TVTStatus[]> {
     const response = await axios.get(url("/api/vtstatuses.json"));
     return response.data.message;
 }
+
+export async function startElection() {
+    const response = await axios.post(url("/voting-process-manager-api/start"))
+        .then( function (response){
+            let message = '';
+            if(response.status == 200) {
+                message += 'Úspešne spustené terminály. (' + response.data.success_terminals_count + ')\n';
+                message += 'Neúspešne spustené terminály. (' + response.data.error_terminals_count + ')';
+                alert(message);
+            } else {
+                alert(response.status)
+            }
+        })
+        .catch(function (error) {
+            alert(error)
+        });
+}
+
+
+export async function stopElection() {
+    const response = await axios.post(url("/voting-process-manager-api/end"))
+        .then( function (response){
+            let message = '';
+            if(response.status == 200) {
+                message += 'Úspešne spustené terminály. (' + response.data.success_terminals_count + ')\n';
+                message += 'Neúspešne spustené terminály. (' + response.data.error_terminals_count + ')';
+                alert(message);
+            } else {
+                alert(response.status)
+            }
+        })
+        .catch(function (error) {
+            alert(error)
+        });
+}
--- admin-frontend/src/pages/home/elections/index.svelte
+++ admin-frontend/src/pages/home/elections/index.svelte
@@ -2,11 +2,12 @@
 import ButtonsContainer from "../../../lib/components/buttons/ButtonsContainer.svelte";
 import Button from "../../../lib/components/buttons/Button.svelte";
 import {url} from "@roxi/routify";
+import {startElection, stopElection} from "../../../api/api";
 </script>
 
 <h1>Voľby</h1>
 <ButtonsContainer>
-    <Button href={$url('/home/elections/start')}>Spustiť voľby</Button>
-    <Button href={$url('/home/elections/stop')}>Ukončiť voľby</Button>
+    <Button on:click={startElection}>Spustiť voľby</Button>
+    <Button on:click={stopElection}>Ukončiť voľby</Button>
     <Button href={$url('/home/elections/pause')}>Pozastaviť voľby</Button>
 </ButtonsContainer>

b69692d495083fd04d9c43765f1e674c440954b1
slatinsky <slatinsky.adam@gmail.com>
2022-02-27 22:31:44 +0100

    EV-131 frontend - added breadcrumb

--- admin-frontend/src/lib/components/header/BreadCrumb.svelte
+++ admin-frontend/src/lib/components/header/BreadCrumb.svelte
@@ -1,51 +1,39 @@
-<!--<script>-->
-
-<!--    import {page} from '@roxi/routify';-->
-<!--    import {onMount} from "svelte";-->
-
-<!--    let paths = [];-->
-
-<!--    function updatePaths(pathname) {-->
-<!--        // // loop url, delimeter /-->
-<!--        // paths = pathname.split('/')-->
-<!--        // paths = paths.filter(path => {-->
-<!--        //         if (path == '')-->
-<!--        //             return false-->
-<!--        //         else-->
-<!--        //             return !["", "admin-frontend"].includes(path);-->
-<!--        //     }-->
-<!--        // )-->
-
-<!--        console.log(paths, $page.url.pathname);-->
-<!--        console.log(paths, $page);-->
-<!--    }-->
-
-<!--    onMount(() => {-->
+<script>
+    let paths = []
+    import {page, url} from '@roxi/routify'
 
 
-<!--        // updatePaths($page.url.pathname);-->
-<!--    });-->
+    function updatePaths(pathname) {
+        paths = pathname.split('/')
 
-<!--    $: updatePaths($page.url.pathname);-->
-<!--</script>-->
-<!--<style lang="scss">-->
-<!--  @import 'node_modules/@id-sk/frontend/govuk/components/breadcrumbs/_breadcrumbs.scss';-->
-<!--</style>-->
+        paths = paths.filter(path => {
+                return !["", "admin-frontend", "gateway", "index"].includes(path);
+            }
+        )
+
+        console.log(paths, $page);
+    }
+
+    $: updatePaths($page.path);
+</script>
+<style lang="scss">
+  @import 'node_modules/@id-sk/frontend/govuk/components/breadcrumbs/_breadcrumbs.scss';
+</style>
 
 
-<!--<div class="govuk-breadcrumbs">-->
-<!--    <ol class="govuk-breadcrumbs__list">-->
-<!--        {#each paths as path, index }-->
-<!--            &lt;!&ndash; if not last&ndash;&gt;-->
-<!--            {#if paths.length !== index  + 1}-->
-<!--                <li class="govuk-breadcrumbs__list-item">-->
-<!--                    <a class="govuk-breadcrumbs__link" href={paths.slice(0, index + 1).join("/")}>{path}</a>-->
-<!--                </li>-->
-<!--            {:else}-->
-<!--                <li class="govuk-breadcrumbs__list-item">-->
-<!--                    {path}-->
-<!--                </li>-->
-<!--            {/if}-->
-<!--        {/each}-->
-<!--    </ol>-->
-<!--</div>-->
+<div class="govuk-breadcrumbs">
+    <ol class="govuk-breadcrumbs__list">
+        {#each paths as path, index }
+            <!-- if not last-->
+            {#if paths.length !== index  + 1}
+                <li class="govuk-breadcrumbs__list-item">
+                    <a class="govuk-breadcrumbs__link" href={$url("/" + paths.slice(0, index + 1).join("/"))}>{path}</a>
+                </li>
+            {:else}
+                <li class="govuk-breadcrumbs__list-item">
+                    {path}
+                </li>
+            {/if}
+        {/each}
+    </ol>
+</div>

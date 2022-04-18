a24590d4e3cee7caac87c2d1eec6319525cea7b9
Marek Ceľuch <celuchmarek@gmail.com>
2022-04-18 09:55:49 +0200

    EV-181 sync service tests wait for statevector

--- synchronization-service/start_test.sh
+++ synchronization-service/start_test.sh
@@ -5,6 +5,11 @@ while ! nc -z web 80; do
     sleep 3;
 done;
 
+while ! nc -z statevector 80; do
+    echo "Waiting for the statevector to start...";
+    sleep 3;
+done;
+
 # generate python vote class from json spec
 datamodel-codegen --url http://web/statevector/config/datamodels.yaml --output /code/src/schemas.py
 
